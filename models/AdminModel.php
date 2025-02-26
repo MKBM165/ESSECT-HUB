cb2208718e94f11d5cd952c5c9ef9bef8036d8b0
Hamdi Dridi <hadridi365@gmail.com>
2025-02-26 16:02:23 +0100

    The essential admin functionalities, including user management and access control, are now fully operational and ready for testing

--- models/AdminModel.php
+++ models/AdminModel.php
@@ -8,8 +8,9 @@ class AdminModel{
     $this->conn=$conn;
   }
 
+//get the admin username from admin table 
 public function get_db_username(){
-  $query="SELECT username FROM admin LIMIT 1";  //get the admin username from admin table 
+  $query="SELECT username FROM admin LIMIT 1";  
   $result=$this->conn->query($query);  //store the query result in $result variable
  
   if ($result->num_rows===1){  //if the result has only one row that means the table is valid 
@@ -19,8 +20,9 @@ public function get_db_username(){
 }
 
 
+//get the admin password from admin table 
  public function get_db_password(){
-   $query="SELECT password FROM admin LIMIT 1"; //get the admin password from admin table 
+   $query="SELECT password FROM admin LIMIT 1"; 
    $result=$this->conn->query($query); //store the query result in $result variable
 
    if($result -> num_rows===1){//if the result has only one row that means the table is valid 
@@ -29,6 +31,32 @@ public function get_db_username(){
    }
    return null;
  }
+ 
 
+//login function (can be used only for admin)
+public function admin_login($username,$password){
+  $db_username=$this->get_db_username();
+  $db_password=$this->get_db_password();
+
+  if($db_username!==null && $db_password!==null){
+    return $username===$db_username && $password===$db_password;//return true if input data === db data and false otherwise
+  }
+  return false;//if password or username are empty in the password  
+}
+
+
+//functions to change admin data in the data base
+
+//change admin username 
+public function update_username($newUsername){
+  $query="UPDATE admin SET username = '$newUsername' LIMIT 1";
+  return $this->conn->query($query); //true if username updated successfully and false otherwise
+}
+
+//change admin password 
+public function update_password($newPassword){
+$query="UPDATE admin SET password = '$newPassword' LIMIT 1";
+return $this->conn->query($query); //true if password updated successfully and false otherwise
+}
 }
 ?>
