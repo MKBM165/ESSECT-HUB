50dc5136d680e773d60570ee8f90275ab36d0f54
Hamdi Dridi <hadridi365@gmail.com>
2025-03-05 00:03:17 +0100

    adding a login controller for users + improve the userData file

--- controllers/Usercontrollers/userData.php
+++ controllers/Usercontrollers/userData.php
@@ -2,6 +2,7 @@
 session_start();
 include('../../models/DBconnection.php');
 include('../../models/UserModel.php');
+include('../../models/ClubModel.php'); 
 
 header('Content-Type: application/json');
 
@@ -11,6 +12,8 @@ if (!isset($_SESSION['username'])) {
 }
 
 $usermodel = new UserModel($conn);
+$clubmodel = new ClubModel($conn); 
+
 $username = $_SESSION['username'];
 $user_id = $usermodel->get_user_id($username);
 
@@ -19,13 +22,21 @@ if (!$user_id) {
     exit;
 }
 
+
+$user_data = $usermodel->get_user_by_id($user_id);
+if (!$user_data) {
+    echo json_encode(['error' => 'User data not found']);
+    exit;
+}
+
 $userdata = [
-    'username' => $usermodel->get_username($user_id),
-    'nom' => $usermodel->get_user_nom($user_id),
-    'prenom' => $usermodel->get_user_prenom($user_id),
+    'username' => $user_data['username'],
+    'nom' => $user_data['nom'],
+    'prenom' => $user_data['prenom'],
     'clubs' => []
 ];
 
+// Getting the clubs the user is a member of
 $club_ids = $usermodel->get_user_clubs($user_id);
 if (!is_array($club_ids)) {
     $club_ids = []; 
@@ -33,12 +44,20 @@ if (!is_array($club_ids)) {
 
 foreach ($club_ids as $club_id) {
     $club_id = (int) $club_id;
-    $result = $conn->query("SELECT club_id, nom FROM club WHERE club_id = $club_id");
     
-    if ($result && $club = $result->fetch_assoc()) {
+    $club_name = $clubmodel->get_club_name($club_id);
+    $club_desc = $clubmodel->get_club_desc($club_id);
+    $club_img = $clubmodel->get_club_img($club_id);
+    $club_email = $clubmodel->get_club_email($club_id);
+
+    if ($club_name) {
+        // Adding club data to the user data array
         $userdata['clubs'][] = [
-            'id' => $club['club_id'],
-            'nom' => $club['nom']
+            'id' => $club_id,
+            'nom' => $club_name,
+            'desc' => $club_desc,
+            'image' => $club_img,
+            'email' => $club_email
         ];
     }
 }
