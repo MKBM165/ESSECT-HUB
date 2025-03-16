28c2a96350f05e6f8ba20a5ee256dd7214a1ceb9
Khalil Laptop <mohamedkhalilbenmrad@gmail.com>
2025-03-16 16:36:20 +0100

    log in as admin fixed issue

--- views/signInView.js
+++ views/signInView.js
@@ -7,48 +7,51 @@ form.addEventListener("submit", function (e) {
   const password = document.getElementById("password");
   if (username.value === adminUsername && password.value === adminPassword) {
     window.location.href = "admin.html";
-  }
-  const formData = new FormData(this); // Get form data
-  formData.append("action", "login");
-  fetch("/ESSECT-HUB/controllers/ClubController.php", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      if (data.success) {
-        window.location.href = "club-home.html";
-      } else {
-        //alert("Login Failed: " + (data.error || "Invalid credentials."));
-
-        fetch("/ESSECT-HUB/controllers/UserController.php", {
-          method: "POST",
-          body: formData,
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-            if (data.success) {
-              window.location.href = "user-home.html";
-            } else {
-              alert("Login Failed: " + (data.error || "Invalid credentials."));
-            }
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-            alert("An error occurred. Please try again.");
-          });
-      }
+  } else {
+    const formData = new FormData(this); // Get form data
+    formData.append("action", "login");
+    fetch("/ESSECT-HUB/controllers/ClubController.php", {
+      method: "POST",
+      body: formData,
     })
-    .catch((error) => {
-      console.error("Error:", error);
-      alert("An error occurred. Please try again.");
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success) {
+          window.location.href = "club-home.html";
+        } else {
+          //alert("Login Failed: " + (data.error || "Invalid credentials."));
+
+          fetch("/ESSECT-HUB/controllers/UserController.php", {
+            method: "POST",
+            body: formData,
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+              if (data.success) {
+                window.location.href = "user-home.html";
+              } else {
+                alert(
+                  "Login Failed: " + (data.error || "Invalid credentials.")
+                );
+              }
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+              alert("An error occurred. Please try again.");
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again.");
+      });
+  }
 });
 
 /*
-else if (username.value.slice(4) === "club") {
+  else if (username.value.slice(4) === "club") {
     fetch("/ESSECT-HUB/controllers/ClubController.php", {
       method: "POST",
       body: formData,
